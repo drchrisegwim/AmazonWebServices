85d284849e3bab8e407bd4f7042b1aa2422c34ba
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-05-03 18:32:29 +0100

    Initial Commit

---
+++ src/AwsS3BucketUpload/Controllers/DocumentsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AwsS3BucketUpload.Models;
+
+namespace AwsS3BucketUpload.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        // GET: Documents
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ValidateAntiForgeryToken, HttpPost]
+        public ActionResult Create(Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                if (document.File != null)
+                {
+                    var file = document.File;
+
+
+
+
+                    Stream st = file.InputStream;
+
+                    string name = file.FileName;
+                    string myBucketName = "test73"; //your s3 bucket name goes here  
+                    string s3DirectoryName = "";
+                    string s3FileName = @name;
+                    bool a;
+                    AmazonUploader myUploader = new AmazonUploader();
+                    a = myUploader.sendMyFileToS3(st, myBucketName, s3DirectoryName, s3FileName);
+                    if (a == true)
+                    {
+                        Response.Write("successfully uploaded");
+
+                    }
+                    else
+                        Response.Write("Error");
+
+
+
+
+
+                    // await _documentService.SaveDocument(model.ToEntity(), file.InputStream, file.FileName, file.ContentType, file.ContentLength);
+
+                    // SuccessNotification(Domain.Messages.Document.Admin.DocumentCreated);
+
+                    return RedirectToAction("Index");
+                }
+                //ErrorNotification(Domain.Messages.Document.Admin.DocumentInvalid);
+
+            }
+
+            return View("Index",document);
+        }
+    }
+}
---
+++ src/AwsS3BucketUpload/Models/Document.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AwsS3BucketUpload.Models
+{
+    public class Document
+    {
+        //public string Name { get; set; }
+        //public string BucketName { get; set; }
+
+        //public string S3DirectoryName { get; set; }
+
+        //public string S3FileName { get; set; }
+
+        //public bool A { get; set; }
+
+
+        [Required]
+        public HttpPostedFileBase File { get; set; }
+        public int FileSize { get; set; }
+
+        [Display(Name = "Mime Type")]
+        public string MimeType { get; set; }
+        public DateTime CreatedOnUtc { get; set; }
+        public string RelativeUrl { get; set; }
+
+        public int Id { get; set; }
+        public string BlobKey { get; set; }
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+    }
+}
